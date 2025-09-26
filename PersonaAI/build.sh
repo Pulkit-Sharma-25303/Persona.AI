627d7ae94dde2564d2f7c198d0e68b1b8b531090
Pulkit Sharma <pulkit2693@gmail.com>
2025-09-26 15:11:44 +0530

    Update build.sh

--- PersonaAI/build.sh
+++ PersonaAI/build.sh
@@ -1,7 +1,12 @@
-# Exit on error
+#!/usr/bin/env bash
+# exit on error
 set -o errexit
 
+# Install all the packages from requirements.txt
 pip install -r requirements.txt
 
+# Collect all static files (CSS, JS, etc.) into one folder
 python manage.py collectstatic --no-input
+
+# Apply any database migrations. This will create your tables.
 python manage.py migrate
